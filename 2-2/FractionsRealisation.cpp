62db5f870015a5dc4577d4b0e441ca6e28dcdabd
RPK <69837756+aK07200@users.noreply.github.com>
2021-05-14 08:50:42 +0300

    Update FractionsRealisation.cpp

--- 2-2/FractionsRealisation.cpp
+++ 2-2/FractionsRealisation.cpp
@@ -19,28 +19,28 @@ int nok(int a, int b) {
 	return a * b / nod(a, b);
 }
 
-Overload::Overload() {
+Fraction::Fraction() {
 	numerator = 0; denaminator = 1;
 };
 
-Overload::Overload(int a1, int b1) {
+Fraction::Fraction(int a1, int b1) {
 	numerator = a1; denaminator = b1;
 };
 
-Overload::Overload(const Overload& lnk) {
+Fraction::Fraction(const Fraction& lnk) {
 	numerator = lnk.numerator; denaminator = lnk.denaminator;
 };
 
-Overload::Overload(Overload&& llnk) {
+Fraction::Fraction(Fraction&& llnk) {
 	numerator = llnk.numerator; denaminator = llnk.denaminator;
 };
 
-std::ostream& operator<< (std::ostream& out, const Overload& ovrl) {
+std::ostream& operator<< (std::ostream& out, const Fraction& ovrl) {
 	out << ovrl.numerator << " / " << ovrl.denaminator << std::endl;
 	return out;
 };
 
-std::istream& operator>> (std::istream& in, Overload& ovrl) {
+std::istream& operator>> (std::istream& in, Fraction& ovrl) {
 	std::string inp, num1 = "", den1 = ""; int posit = 0;
 	in >> inp;
 
@@ -58,91 +58,91 @@ std::istream& operator>> (std::istream& in, Overload& ovrl) {
 	return in;
 };
 
-Overload operator* (const Overload& ovrl1, const Overload& ovrl2) {
+Fraction operator* (const Fraction& ovrl1, const Fraction& ovrl2) {
 	int numer, denam, noD;
 	numer = (ovrl1.numerator * ovrl2.numerator);
 	denam = (ovrl1.denaminator * ovrl2.denaminator);
 	noD = nod(numer, denam);
-	return Overload(numer / noD, denam / noD);
+	return Fraction(numer / noD, denam / noD);
 };
 
-Overload operator/ (const Overload& ovrl1, const Overload& ovrl2) {
+Fraction operator/ (const Fraction& ovrl1, const Fraction& ovrl2) {
 	int numer, denam, noD;
 	numer = (ovrl1.numerator * ovrl2.denaminator);
 	denam = (ovrl1.denaminator * ovrl2.numerator);
 	noD = nod(numer, denam);
-	return Overload(numer /= noD, denam /= noD);
+	return Fraction(numer /= noD, denam /= noD);
 };
 
-Overload operator- (const Overload& ovrl1, const Overload& ovrl2) {
+Fraction operator- (const Fraction& ovrl1, const Fraction& ovrl2) {
 	int numer, denam, noD, noK;
 	noK = nok(ovrl1.denaminator, ovrl2.denaminator);
 	numer = ((ovrl1.numerator * (noK / ovrl1.denaminator) - (ovrl2.numerator * (noK / ovrl2.denaminator))));
 	denam = noK;
 	noD = nod(numer, denam);
-	return Overload(numer /= noD, denam /= noD);
+	return Fraction(numer /= noD, denam /= noD);
 };
 
-Overload operator+ (const Overload& ovrl1, const Overload& ovrl2) {
+Fraction operator+ (const Fraction& ovrl1, const Fraction& ovrl2) {
 	int numer, denam, noD, noK;
 	noK = nok(ovrl1.denaminator, ovrl2.denaminator);
 	numer = ((ovrl1.numerator * (noK / ovrl1.denaminator) + (ovrl2.numerator * (noK / ovrl2.denaminator))));
 	denam = noK;
 	noD = nod(numer, denam);
-	return Overload(numer /= noD, denam /= noD);
+	return Fraction(numer /= noD, denam /= noD);
 };
 
-Overload Overload::operator= (Overload ovrl) {
+Fraction Fraction::operator= (Fraction ovrl) {
 	this->numerator = ovrl.numerator;
 	this->denaminator = ovrl.denaminator;
 	return *this;
 }
 
-Overload Overload::operator+=(const Overload& ovrl)
+Fraction Fraction::operator+=(const Fraction& ovrl)
 {
 	operator=(operator+(*this, ovrl));
 	return *this;
 }
 
-Overload Overload::operator-=(const Overload& ovrl)
+Fraction Fraction::operator-=(const Fraction& ovrl)
 {
 	operator=(operator-(*this, ovrl));
 	return *this;
 }
 
 
-Overload Overload::operator*=(const Overload& ovrl)
+Fraction Fraction::operator*=(const Fraction& ovrl)
 {
 	operator=(operator*(*this, ovrl));
 	return *this;
 }
 
-Overload Overload::operator/=(const Overload& ovrl)
+Fraction Fraction::operator/=(const Fraction& ovrl)
 {
 	operator=(operator/(*this, ovrl));
 	return *this;
 }
 
-bool operator== (const Overload& ovrl1, const Overload& ovrl2) {		
+bool operator== (const Fraction& ovrl1, const Fraction& ovrl2) {		
 	return ((double)(ovrl1.numerator / ovrl1.denaminator) == (double)(ovrl2.numerator / ovrl2.denaminator));
 };
 
-bool operator> (const Overload& ovrl1, const Overload& ovrl2) {		
+bool operator> (const Fraction& ovrl1, const Fraction& ovrl2) {		
 	return ((double)(ovrl1.numerator / ovrl1.denaminator) > (double)(ovrl2.numerator / ovrl2.denaminator));
 };
 
-bool operator< (const Overload& ovrl1, const Overload& ovrl2) {		
+bool operator< (const Fraction& ovrl1, const Fraction& ovrl2) {		
 	return ((double)(ovrl1.numerator / ovrl1.denaminator) < (double)(ovrl2.numerator / ovrl2.denaminator));
 };
 
-bool operator>= (const Overload& ovrl1, const Overload& ovrl2) {		
+bool operator>= (const Fraction& ovrl1, const Fraction& ovrl2) {		
 	return ((double)(ovrl1.numerator / ovrl1.denaminator) >= (double)(ovrl2.numerator / ovrl2.denaminator));
 };
 
-bool operator<= (const Overload& ovrl1, const Overload& ovrl2) {
+bool operator<= (const Fraction& ovrl1, const Fraction& ovrl2) {
 	return ((double)(ovrl1.numerator / ovrl1.denaminator) <= (double)(ovrl2.numerator / ovrl2.denaminator));
 };
 
-bool operator!= (const Overload& ovrl1, const Overload& ovrl2) {
+bool operator!= (const Fraction& ovrl1, const Fraction& ovrl2) {
 	return ((double)(ovrl1.numerator / ovrl1.denaminator) != (double)(ovrl2.numerator / ovrl2.denaminator));
 };
